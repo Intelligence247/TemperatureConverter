7ddf28e7d1841aebceb474c6698ba860d948c396
Intelligence247 <115150772+Intelligence247@users.noreply.github.com>
2023-03-25 01:04:46 +0100

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,27 @@ import './App.css'
 function App() {
   const [ans, setAns] = useState('')
   const [selects, setSelects] = useState(0)
+  const [selects2, setSelects2] = useState(0)
   const [inputs, setInputs] = useState('')
   const [img, setImg] = useState('')
+  const [selectValid, setSelectValid] = useState(0)
 console.log(inputs.length)
 console.log(ans.length)
 const handselects=(e)=>{
   setSelects(e.target.value)
   
 }
+const handleselects2=(e)=>{
+  setSelects2(e.target.value)
+}
+
 const handleclicks=()=>{
-  setAns(`${selects==='celsius'?(((inputs*9)+160)/5) : (((inputs-32)*5)/9)}`)
-  setImg(selects === 'celsius' ? "/media/fahrent2.png" : "/media/celsius.png")
+  setAns(`${selects==='celsius' && selects2=== 'fahrenheit'?(((inputs*9)+160)/5) : selects==='fahrenheit' && selects2==='celsius' ? (((inputs-32)*5)/9): selects === 'kelvin' && selects2==='fahrenheit'?((inputs-273)*1.8+32):selects === 'kelvin' && selects2 ==='celsius'?((inputs-273)):selects=== 'celsius' && selects2==='kelvin'?((parseInt(inputs)+273)):selects==='fahrenheit' && selects2==='kelvin'?((parseInt(inputs)+459.4)/1.8):selects==='default'||selects2==='default'?'':selects==="kelvin"&&selects2==="kelvin"?(inputs):selects==="celsius"&&selects2==="celsius"?(inputs):selects==="fahrenheit"&&selects2==="fahrenheit"?(inputs):''}`)
+
+
+  setImg(selects2 === 'fahrenheit' ? "/media/fahrent2.png" : selects2== "celsius"?"/media/celsius.png": selects2 ==="kelvin"?"/media/kelvin.jfif":'')
+
+  
 }
   return (
     <div className="body">
@@ -29,10 +39,14 @@ const handleclicks=()=>{
    />
    <div className={`${inputs.length>0?'valid':'invalid'}`}>Inputbox must not be empty</div>
 </label>
-<div className="selects">
-  <p>Type</p>
+<div className="selects flex justify-center items-center gap-8 relative">
+  <section>
+  <p>Converting From</p>
   <select name=""
   onChange={(e)=>handselects(e)}>
+    <option value="default">
+      ...Converting from
+    </option>
      <option value="fahrenheit">
       Fahrenheit
     </option>
@@ -40,15 +54,40 @@ const handleclicks=()=>{
     <option value="celsius">
       Celsius
     </option>
-   
+    <option value="kelvin">
+    Kelvin
+   </option>
   </select>
+  </section>
+<section>
+  <p>Converting too</p>
+  <select name=""
+  className=''
+  onChange={(e)=>handleselects2(e)}>
+     <option value="default">
+     ...Converting to
+    </option>
+     <option value="fahrenheit">
+      Fahrenheit
+    </option>
+
+    <option value="celsius">
+      Celsius
+    </option>
+   <option value="kelvin">
+    Kelvin
+   </option>
+  </select>
+  <p className='absolute left-[4rem] '>You must choose from both side</p>
+  </section>
+
   </div>
   <button
   onClick={()=>handleclicks()}
   >Convert</button>
   </div>
   <div className="ansW ">
-    <p>Result</p>
+    <p>Result:</p>
     <div className="resultW">
     <div className="final_ans">{inputs.length < 1 ?'': ans }</div>
     <img src={img}
